9f5dae14752653b267a32d9d5b6292e161658930
rumabe97 <rumabe97@gmail.com>
2024-11-27 16:28:11 +0100

    Test process.env variables

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+        [
+            'transform-inline-environment-variables',
+            {
+                include: ['API_URL', 'API_AUTH'],
+            },
+        ],
+    ],
 };
